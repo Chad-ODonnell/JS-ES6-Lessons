1b08be3ebfc304a6d6921b3ec6a7c6af7754c3c6
Chad O'Donnell <76896489+Chad-ODonnell@users.noreply.github.com>
2024-01-22 14:29:40 -0600

    Create Beginner_arrPop.js

---
+++ Beginner_arrPop.js
@@ -0,0 +1,8 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+// Creates a new Array to store removed element values.
+const removedElements = chores.pop();
+
+// You'll notice the difference here.
+console.log(chores); // first 4 elements
+console.log(removedElements); // just 'mop floor'.
